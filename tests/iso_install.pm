fcb51f1358e5db2090a244fc22f69fdd11422517
davidtclin <davidtclin58@gmail.com>
2025-03-24 15:43:22 +0800

    Update tests and needles for Harvester v1.5.0
    Signed-off-by: davidtclin <davidtclin58@gmail.com>

--- tests/iso_install.pm
+++ tests/iso_install.pm
@@ -8,13 +8,13 @@ sub run {
     # the bootloader screen will timeout itself
     # assert_screen "bootloader", 15;
 
-    assert_screen "confirm_hardware_check", 45;
+    assert_screen "confirm_hardware_check_150", 45;
 
     # select to accept Hardware checks
     send_key "ret";
    
     ## Page: Choose installation mode page
-    assert_screen "select_installation_mode";
+    assert_screen "choose_install_mode_150";
 
     # send_key "down";
     
@@ -27,7 +27,7 @@ sub run {
     send_key "ret"; #Click "enter" on the 
 
     ## Page: Disk selection page 
-    assert_screen "choose_installation_disk";
+    assert_screen "choose_install_disk_150";
 
     send_key "ret"; #Click "enter" on the Choose installation disk
 
@@ -39,7 +39,7 @@ sub run {
 
 
     # Page Configure Password 
-    assert_screen "configure_the_password";
+    assert_screen "configure_password_150";
 
     type_string "passwd"; #Enter value in the password field
     send_key "ret"; #Click enter 
@@ -72,11 +72,21 @@ sub run {
 
     send_key "ret"; #Click enter on Bond Mode field
 
-    assert_screen "image_create_config_management-NIC";
+    assert_screen "configure_network_150";
 
     send_key "ret"; #Click enter use the IPv4 Method
 
     sleep 10;
+
+    # Configure cluster network
+
+    assert_screen "configure_cluster_network_150";
+
+    send_key "ret"; #Click enter to input Pod CIDR
+
+    send_key "ret"; #Click enter to input Service CIDR
+
+    send_key "ret"; #Click enter to input Cluster DNS IP
 
     # Page Configure hostname
 
@@ -88,67 +98,68 @@ sub run {
     # Configure DNS Servers Page # 
 
     sleep 5;
-    assert_screen "configure_dns_new";
+    assert_screen "configure_dns_150";
     send_key "ret"; #Click enter on DNS Servers field and procced to next page
     
     # Configure VIP Page #
 
     sleep 5;
-
-    assert_screen "configure_vip_dhcp", 60;
+    assert_screen "configure_vip_dhcp_150", 60;
+    send_key "ret"; #Click enter on MAC Address
     send_key "ret"; #Click enter on VIP mode field
 
-    assert_screen "dhcp_vip_assigned";
+    assert_screen "dhcp_vip_assigned_150";
 
     send_key "ret"; #Click enter on VIP field to proceed to next page
 
-
     # Configure Cluster token #
 
-    assert_screen "configure_cluster_token_empty";
+    assert_screen "configure_cluster_token_empty_150";
 
     type_string "harvester"; #Enter value in Cluster token field
-    
-    assert_screen "configure_cluster_token_harvester";
+
+    assert_screen "configure_cluster_token_harvester_150";
 
     send_key "ret"; #Click enter on Cluster token field and procced to next page
 
+
     # Configure the password to access the node page#
     
-    assert_screen "configure_node_password";
+    assert_screen "configure_password_150";
 
-    type_string "passwd"; #Enter value in Password field
-    send_key "ret";
+    type_string "passwd"; #Enter value in the password field
+    send_key "ret"; #Click enter 
 
-    type_string "passwd"; #Enter value in Confirm password field
-    send_key "ret"; #Click enter on Confirm password field and procced to next page
+    type_string "passwd"; #Enter value in the confirm password field
+    send_key "ret"; #Click enter to proceed
 
     # Optional: Configure NTP Servers page#
-    assert_screen "configure_ntp_server_140";
+    assert_screen "configure_ntp_server_150";
     send_key "ret"; #Click enter on NTP Servers field and procced to next page
 
     # Optional: Configure proxy page#
-    assert_screen "configure_proxy";
+    assert_screen "configure_proxy_150";
     send_key "ret"; #Click enter on Proxy address field and procced to next page
 
     # Optional: import SSH keys page#
-    assert_screen "import_ssh_keys";
+    assert_screen "import_ssh_keys_150";
     send_key "ret"; #Click enter on Http URL field and procced to next page
 
     # Optional: remote Harvester config page#
-    assert_screen "remote_harvester_config";
+    assert_screen "remote_harvester_config_150";
     send_key "ret"; #Click enter on Http URL field and procced to next page
 
     # Confirm installation options page#
-    assert_screen "confirm_install_option_v140";
+    assert_screen "iso_confirm_install_option_150";
     send_key "ret"; #Click enter on Yes field and ready to install harvester
+    ##assert_screen "confirm_install_option_v140";
+    ##send_key "ret"; #Click enter on Yes field and ready to install harvester
 
     # Confirm start the console setting progress
-    assert_screen "installation_ongoing_image_v140", 900;
-
+    assert_screen "iso_config_ongoing_150", 900;
 
     # Confirm can successfully install Harvester
-    assert_screen "console_install_success_v121", 600;
+    assert_screen "console_install_success_150", 900;
 
 }
 
